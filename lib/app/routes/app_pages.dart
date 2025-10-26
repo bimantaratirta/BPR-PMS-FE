0600770745063b15e6983cf81282ff91db2f432c
mhndkptr <muhamadhendika77@gmail.com>
2025-10-26 17:23:13 +0700

    chore: adjust report view for direksi & init office view

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -55,22 +55,10 @@ class AppPages {
   static const INITIAL = Routes.SPLASH_SCREEN;
 
   static final routes = [
-    GetPage(
-        name: _Paths.HOME,
-        page: () => const HomeView(),
-        binding: HomeBinding()),
-    GetPage(
-        name: _Paths.SPLASH_SCREEN,
-        page: () => const SplashScreenView(),
-        binding: SplashScreenBinding()),
-    GetPage(
-        name: _Paths.AUTH + _Paths.LOGIN,
-        page: () => const AuthLoginView(),
-        binding: AuthLoginBinding()),
-    GetPage(
-        name: _Paths.AUTH + _Paths.REGISTER,
-        page: () => const AuthRegisterView(),
-        binding: AuthRegisterBinding()),
+    GetPage(name: _Paths.HOME, page: () => const HomeView(), binding: HomeBinding()),
+    GetPage(name: _Paths.SPLASH_SCREEN, page: () => const SplashScreenView(), binding: SplashScreenBinding()),
+    GetPage(name: _Paths.AUTH + _Paths.LOGIN, page: () => const AuthLoginView(), binding: AuthLoginBinding()),
+    GetPage(name: _Paths.AUTH + _Paths.REGISTER, page: () => const AuthRegisterView(), binding: AuthRegisterBinding()),
     GetPage(
       name: _Paths.MAIN,
       page: () => const MainView(),
@@ -86,10 +74,7 @@ class AppPages {
               page: () => const ProfileProfileChangePasswordView(),
               binding: ProfileProfileChangePasswordBinding(),
             ),
-            GetPage(
-                name: "/detail",
-                page: () => const ProfileProfileDetailView(),
-                binding: ProfileProfileDetailBinding()),
+            GetPage(name: "/detail", page: () => const ProfileProfileDetailView(), binding: ProfileProfileDetailBinding()),
           ],
         ),
         GetPage(
@@ -97,23 +82,14 @@ class AppPages {
           page: () => const NasabahView(),
           binding: NasabahBinding(),
           children: [
-            GetPage(
-                name: "/create",
-                page: () => const NasabahNasabahCreateView(),
-                binding: NasabahNasabahCreateBinding()),
-            GetPage(
-                name: '/:id',
-                page: () => const NasabahNasabahDetailView(),
-                binding: NasabahNasabahDetailBinding()),
+            GetPage(name: "/create", page: () => const NasabahNasabahCreateView(), binding: NasabahNasabahCreateBinding()),
+            GetPage(name: '/:id', page: () => const NasabahNasabahDetailView(), binding: NasabahNasabahDetailBinding()),
             GetPage(
               name: '/:id/report',
               page: () => const NasabahNasabahCreateReportView(),
               binding: NasabahNasabahCreateReportBinding(),
             ),
-            GetPage(
-                name: '/:id/edit',
-                page: () => const NasabahNasabahEditView(),
-                binding: NasabahNasabahEditBinding()),
+            GetPage(name: '/:id/edit', page: () => const NasabahNasabahEditView(), binding: NasabahNasabahEditBinding()),
           ],
         ),
         GetPage(
@@ -121,10 +97,7 @@ class AppPages {
           page: () => const ReportView(),
           binding: ReportBinding(),
           children: [
-            GetPage(
-                name: "/:id",
-                page: () => const ReportReportDetailView(),
-                binding: ReportReportDetailBinding()),
+            GetPage(name: "/:id", page: () => const ReportReportDetailView(), binding: ReportReportDetailBinding()),
             GetPage(
               name: '/:id/slo-review',
               page: () => const ReportReportSloReviewView(),
@@ -146,30 +119,16 @@ class AppPages {
           name: _Paths.LO,
           page: () => const LoView(),
           binding: LoBinding(),
-          children: [
-            GetPage(
-                name: "/:id",
-                page: () => const LoLoDetailView(),
-                binding: LoLoDetailBinding())
-          ],
+          children: [GetPage(name: "/:id", page: () => const LoLoDetailView(), binding: LoLoDetailBinding())],
         ),
         GetPage(
           name: _Paths.SLO,
           page: () => const SloView(),
           binding: SloBinding(),
-          children: [
-            GetPage(
-                name: "/:id",
-                page: () => const SloSloDetailView(),
-                binding: SloSloDetailBinding())
-          ],
+          children: [GetPage(name: "/:id", page: () => const SloSloDetailView(), binding: SloSloDetailBinding())],
         ),
+        GetPage(name: _Paths.OFFICE, page: () => const OfficeView(), binding: OfficeBinding()),
       ],
-    ),
-    GetPage(
-      name: _Paths.OFFICE,
-      page: () => const OfficeView(),
-      binding: OfficeBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -12,36 +12,29 @@ abstract class Routes {
   static const REPORT = _Paths.REPORT;
   static const LO = _Paths.LO;
   static const SLO = _Paths.SLO;
+  static const OFFICE = _Paths.OFFICE;
 
   // Nasabah Nested Routes
-  static String nasabahDetail(String id) =>
-      '${_Paths.MAIN}${_Paths.NASABAH}/$id';
-  static String nasabahEdit(String id) =>
-      '${_Paths.MAIN}${_Paths.NASABAH}/$id/edit';
-  static String nasabahReport(String id) =>
-      '${_Paths.MAIN}${_Paths.NASABAH}/$id/report';
+  static String nasabahDetail(String id) => '${_Paths.MAIN}${_Paths.NASABAH}/$id';
+  static String nasabahEdit(String id) => '${_Paths.MAIN}${_Paths.NASABAH}/$id/edit';
+  static String nasabahReport(String id) => '${_Paths.MAIN}${_Paths.NASABAH}/$id/report';
   static String nasabahCreate() => '${_Paths.MAIN}${_Paths.NASABAH}/create';
 
   // Profile Nested Routes
   static String profileDetail() => '${_Paths.MAIN}${_Paths.PROFILE}/detail';
-  static String profileChangePassword() =>
-      '${_Paths.MAIN}${_Paths.PROFILE}/change-password';
+  static String profileChangePassword() => '${_Paths.MAIN}${_Paths.PROFILE}/change-password';
 
   // Report Nested Routes
   static String reportDetail(String id) => '${_Paths.MAIN}${_Paths.REPORT}/$id';
-  static String reportSloReview(String id) =>
-      '${_Paths.MAIN}${_Paths.REPORT}/$id/slo-review';
-  static String reportSloEvaluation(String id) =>
-      '${_Paths.MAIN}${_Paths.REPORT}/$id/slo-evaluation';
-  static String reportAmEvaluationReview(String id) =>
-      '${_Paths.MAIN}${_Paths.REPORT}/$id/am-evaluation-review';
+  static String reportSloReview(String id) => '${_Paths.MAIN}${_Paths.REPORT}/$id/slo-review';
+  static String reportSloEvaluation(String id) => '${_Paths.MAIN}${_Paths.REPORT}/$id/slo-evaluation';
+  static String reportAmEvaluationReview(String id) => '${_Paths.MAIN}${_Paths.REPORT}/$id/am-evaluation-review';
 
   // LO Nested Routes
   static String loDetail(String id) => '${_Paths.MAIN}${_Paths.LO}/$id';
 
   // SLO Nested Routes
   static String sloDetail(String id) => '${_Paths.MAIN}${_Paths.SLO}/$id';
-  static const OFFICE = _Paths.OFFICE;
 }
 
 abstract class _Paths {
