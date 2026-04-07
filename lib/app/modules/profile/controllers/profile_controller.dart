1c4a83907f4d2bb8b7cbfca5c75df5f8c029744d
Muhammad Hendika Putra <muhamadhendika77@gmail.com>
2026-04-08 05:34:25 +0700

    feat: action request location in role AM & Direksi

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -1,10 +1,29 @@
 import 'package:bpr_pms/app/modules/auth/controllers/auth_controller.dart';
 import 'package:bpr_pms/app/modules/main/controllers/main_controller.dart';
 import 'package:bpr_pms/app/routes/app_pages.dart';
+import 'package:bpr_pms/app/data/modules/location/location_service.dart';
+import 'package:bpr_pms/app/widgets/build_custom_snackbar.dart';
 import 'package:get/get.dart';
 
 class ProfileController extends GetxController {
   final AuthController authController = Get.find<AuthController>();
+  final LocationService _locationService = LocationService();
+  final RxBool isRequestingLocation = false.obs;
+
+  Future<void> requestAllLocation() async {
+    isRequestingLocation.value = true;
+    
+    final response = await _locationService.requestAllLocation();
+    
+    isRequestingLocation.value = false;
+    Get.back(); // Close dialog
+
+    if (response.status == 'success' || (response.code != null && response.code! >= 200 && response.code! < 300)) {
+      CustomSnackbar(message: "Berhasil meminta lokasi semua pengguna", type: CustomSnackbarType.success).show(Get.context!);
+    } else {
+      CustomSnackbar(message: response.message ?? "Gagal meminta lokasi", type: CustomSnackbarType.error).show(Get.context!);
+    }
+  }
 
   void handleLogout() async {
     final mainController = Get.find<MainController>();
